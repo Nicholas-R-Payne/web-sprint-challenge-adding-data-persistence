118093eeeacd8c293b5071f2202ac9af07dbdcda
DarkedgeTH <darkedgeth@gmail.com>
2022-04-21 19:32:10 -0400

    wired routers

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,11 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
