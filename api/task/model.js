e1b90a2d02f729f5c96c0c51422e3c0de232accd
DarkedgeTH <darkedgeth@gmail.com>
2022-04-21 22:42:58 -0400

    POST request for /api/tasks working

--- api/task/model.js
+++ api/task/model.js
@@ -14,4 +14,9 @@ function getTasks() {
       );
 }
 
-module.exports = { getTasks }
+async function createTask(task) {
+    const [task_id] = await db('tasks').insert(task)
+    return getTasks().where({ task_id }).first()
+}
+
+module.exports = { getTasks, createTask }
--- api/task/router.js
+++ api/task/router.js
@@ -11,11 +11,26 @@ router.get('/', (req, res, next) => {
             task_notes: task.task_notes,
             task_completed: Boolean(task.task_completed),
             project_name: task.project_name,
-            project_description: task.project_description,
+            project_description: task.project_description
           }
         })
         res.status(200).json(resp);
       })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  Task.createTask(req.body)
+    .then(task => {
+      let resp = {
+        task_description: task.task_description,
+        task_notes: task.task_notes,
+        task_completed: Boolean(task.task_completed),
+        project_name: task.project_name,
+        project_description: task.project_description
+      }
+      res.status(200).json(resp)
+    })
     .catch(next)
 })
 
