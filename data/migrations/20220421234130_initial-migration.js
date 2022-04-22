fe51303cc26ec28709bdd4037425444b8b7cdda5
DarkedgeTH <darkedgeth@gmail.com>
2022-04-21 20:00:37 -0400

    set up resources and tasks tables

--- data/migrations/20220421234130_initial-migration.js
+++ data/migrations/20220421234130_initial-migration.js
@@ -3,16 +3,26 @@ exports.up = async function(knex) {
   await knex.schema
     .createTable('projects', tbl => {
       tbl.increments('project_id')
-      tbl.string('project_name') .notNullable()
+      tbl.string('project_name').notNullable()
       tbl.string('project_description')
-      tbl.integer('project_completed').notNullable();
+      tbl.integer('project_completed').notNullable()
     })
     .createTable('resources', tbl => {
-        tbl.increments()    
-      })
+      tbl.increments('resource_id')
+      tbl.string('resource_name').notNullable().unique()
+      tbl.string('project_description')   
+    })
     .createTable('tasks', tbl => {
-        tbl.increments()    
-      })
+      tbl.increments('task_id')
+      tbl.string('task_description').notNullable()
+      tbl.string('task_notes')
+      tbl.integer('task_completed').notNullable()
+      tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+    })
 };
 
 
